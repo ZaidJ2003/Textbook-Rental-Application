f3003a1fc0269dc3bf9aa275a41af0068738ad6e
ZaidJ <zaidjebril@icloud.com>
2024-11-29 02:18:21 -0500

    fixing err

---
+++ src/models.py
@@ -0,0 +1,262 @@
+from datetime import datetime, timedelta
+from flask_sqlalchemy import SQLAlchemy
+from itsdangerous.url_safe import URLSafeTimedSerializer as Serializer
+from sqlalchemy.sql import func
+from flask_login import UserMixin
+from sqlalchemy.dialects.postgresql import UUID
+import uuid
+import os
+from uuid import uuid4
+
+db = SQLAlchemy()
+
+# User table
+class users(db.Model, UserMixin):
+        __tablename__ = 'users'
+        user_id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+        first_name = db.Column(db.String(255), nullable=False)
+        last_name = db.Column(db.String(255), nullable=False)
+        email = db.Column(db.String(255), nullable=False, unique=True)
+        username = db.Column(db.String(255), nullable=False, unique=True)
+        password = db.Column(db.String(255), nullable=False)
+        registration_date = db.Column(db.DateTime, nullable=True, default=func.now())
+        profile_picture = db.Column(db.String(255), nullable=True)
+        phone_number = db.Column(db.String(20), nullable=False)
+
+        def __init__(self, first_name, last_name, email, phone_number, username, password, profile_picture):
+                self.first_name = first_name
+                self.last_name = last_name
+                self.email = email
+                self.username = username
+                self.password = password
+                self.profile_picture = profile_picture
+                self.phone_number = phone_number
+        
+        def get_reset_token(self, expires_sec=900):
+                # must have app_secret key variable in env file
+                app_secret_key = os.getenv('APP_SECRET_KEY')
+                if app_secret_key:
+                        s = Serializer(app_secret_key)
+                        token = s.dumps({'user_id' : str(self.user_id)})
+                        if isinstance(token, bytes):
+                                token = token.decode()
+                        return token
+                return None
+        
+        @staticmethod
+        def verify_reset_token(token):
+                # must have app_secret key variable in env file
+                app_secret_key = os.getenv('APP_SECRET_KEY')
+                if app_secret_key:
+                        s = Serializer(app_secret_key)
+                        try:
+                                user_id = s.loads(token)['user_id']
+                        except:
+                                return None
+                        return users.query.get(user_id)
+                return None
+
+class UnverifiedUsers(db.Model):
+        __tablename__ = 'unverified_users'
+        unverified_user_id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+        first_name = db.Column(db.String(255), nullable=False)
+        last_name = db.Column(db.String(255), nullable=False)
+        email = db.Column(db.String(255), nullable=False, unique=True)
+        username = db.Column(db.String(255), nullable=False, unique=True)
+        password = db.Column(db.String(255), nullable=False)
+        registration_date = db.Column(db.DateTime, nullable=True, default=func.now())
+        profile_picture = db.Column(db.String(255), nullable=True)
+        phone_number = db.Column(db.String(20), nullable=False)
+
+        def __init__(self, first_name, last_name, email, phone_number, username, password, profile_picture):
+                self.first_name = first_name
+                self.last_name = last_name
+                self.email = email
+                self.username = username
+                self.password = password
+                self.profile_picture = profile_picture
+                self.phone_number = phone_number
+
+# Textbook table
+class Textbook(db.Model):
+        __tablename__ = 'textbooks'
+        textbook_id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+        owners_user_id = db.Column(UUID(as_uuid=True), db.ForeignKey('users.user_id'), nullable=False)
+        title = db.Column(db.String(255), nullable=False)
+        description = db.Column(db.Text, nullable=False)
+        image_url = db.Column(db.String(255))
+        price = db.Column(db.Numeric(10, 2), nullable=False)
+        created_at = db.Column(db.DateTime, default=func.now())
+
+        owner = db.relationship('users', foreign_keys=[owners_user_id], backref='textbooks')
+
+# Cart table
+class Cart(db.Model):
+        __tablename__ = 'carts'
+        cart_id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+        user_id = db.Column(UUID(as_uuid=True), db.ForeignKey('users.user_id'), nullable=False)
+
+        def __init__(self, user_id):
+                self.user_id = user_id
+
+# Cart items table
+class CartItem(db.Model):
+        __tablename__ = 'cart_items'
+        item_id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+        cart_id = db.Column(UUID(as_uuid=True), db.ForeignKey('carts.cart_id'), nullable=False)
+        textbook_id = db.Column(UUID(as_uuid=True), db.ForeignKey('textbooks.textbook_id'), nullable=False)
+        quantity = db.Column(db.Integer, nullable=False)
+        checkout_type = db.Column(db.String(10), nullable=False, default='buy')
+        duration = db.Column(db.Integer, nullable=True, default=None) 
+
+        def __init__(self, cart_id, textbook_id, quantity, checkout_type='buy', duration = None):
+                self.cart_id = cart_id
+                self.textbook_id = textbook_id
+                self.quantity = quantity
+                self.checkout_type = checkout_type
+                self.duration = duration
+
+# Conversations table
+class Conversations(db.Model):
+        __tablename__ = 'conversations'
+        conversation_id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+        sender_user_id = db.Column(UUID(as_uuid=True), db.ForeignKey('users.user_id'), nullable=False)
+        receiver_user_id = db.Column(UUID(as_uuid=True), db.ForeignKey('users.user_id'), nullable=False)
+        textbook_id = db.Column(UUID(as_uuid=True), db.ForeignKey('textbooks.textbook_id'), nullable=False)
+        meetup_location = db.Column(db.String(255), nullable=True)
+
+        messages = db.relationship('Messages', backref='conversation', lazy=True)
+
+        sender = db.relationship('users', foreign_keys=[sender_user_id], backref='sent_conversations')
+
+        receiver = db.relationship('users', foreign_keys=[receiver_user_id], backref='received_conversations')
+
+        textbook = db.relationship('Textbook', foreign_keys=[textbook_id])
+
+        def __init__(self, sender_user_id, receiver_user_id, textbook_id, meetup_location=None):
+                self.sender_user_id = sender_user_id
+                self.receiver_user_id = receiver_user_id
+                self.textbook_id = textbook_id
+                self.meetup_location = meetup_location
+
+# Messages table
+class Messages(db.Model):
+        __tablename__ = 'messages'
+        message_id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+        user_id = db.Column(UUID(as_uuid=True), db.ForeignKey('users.user_id'), nullable=False)
+        conversation_id = db.Column(UUID(as_uuid=True), db.ForeignKey('conversations.conversation_id'), nullable=False)
+        message_text = db.Column(db.String(255), nullable=False)
+        created_at = db.Column(db.DateTime, nullable=False, default=func.now())
+
+        def __init__(self, user_id, conversation_id, message_text):
+                self.user_id = user_id
+                self.conversation_id = conversation_id
+                self.message_text = message_text
+
+# Verification Codes table
+class VerificationCodes(db.Model):
+        __tablename__ = 'verification_codes'
+        code_id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+        user_id = db.Column(UUID(as_uuid=True), db.ForeignKey('users.user_id'), nullable=False)
+        verification_code = db.Column(db.String(255), nullable=False)
+        expiration_timestamp = db.Column(db.DateTime, nullable=False)
+        is_used = db.Column(db.Boolean, default=False)
+
+        def __init__(self, user_id, verification_code, expiration_date):
+                self.user_id = user_id
+                self.verification_code = verification_code
+                self.expiration_timestamp = expiration_date
+
+# Order table
+class Order(db.Model):
+        __tablename__ = 'orders'
+        order_id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+        user_id = db.Column(UUID(as_uuid=True), db.ForeignKey('users.user_id'), nullable=False)
+        status = db.Column(db.String(10), default='pending')
+        price = db.Column(db.Numeric(10, 2), nullable=False)
+        order_date = db.Column(db.DateTime, nullable=False, default=func.now())
+
+        orderItems = db.relationship('OrderItem', back_populates='order', lazy=True)
+
+        def __init__(self, user_id, price, status='pending', order_id = None):
+                self.order_id = order_id or uuid4()
+                self.user_id = user_id
+                self.status = status
+                self.price = price
+
+# Cart items table
+class OrderItem(db.Model):
+        __tablename__ = 'order_items'
+        item_id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+        order_id = db.Column(UUID(as_uuid=True), db.ForeignKey('orders.order_id'), nullable=False)
+        textbook_id = db.Column(UUID(as_uuid=True), db.ForeignKey('textbooks.textbook_id'), nullable=False)
+        quantity = db.Column(db.Integer, nullable=False)
+        due_date = db.Column(db.DateTime, nullable=True)
+
+        order = db.relationship('Order', back_populates='orderItems', lazy=True)
+        textbook = db.relationship('Textbook', foreign_keys=[textbook_id], lazy=True)
+
+        def __init__(self, order_id, textbook_id, quantity, due_date=None):
+                self.order_id = order_id
+                self.textbook_id = textbook_id
+                self.quantity = quantity
+                self.due_date = due_date
+
+class Rating(db.Model):
+        __tablename__ = 'ratings' 
+        rating_id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+        user_id = db.Column(UUID(as_uuid=True), db.ForeignKey('users.user_id'), nullable=False)
+        textbook_id = db.Column(UUID(as_uuid=True), db.ForeignKey('textbooks.textbook_id'), nullable=False)
+        rating = db.Column(db.Integer, nullable=False)
+        comment = db.Column(db.String(255), nullable=False)
+
+        textbook = db.relationship('Textbook', foreign_keys=[textbook_id], backref='ratings', lazy=True)
+
+        def __init__(self, user_id, textbook_id, rating, comment=None):
+                self.user_id = user_id
+                self.textbook_id = textbook_id
+                self.rating = rating
+                self.comment = comment
+
+class Meetup(db.Model):
+        __tablename__ = 'meetups'
+        meeting_id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+        user_id = db.Column(UUID(as_uuid=True), db.ForeignKey('users.user_id', ondelete='CASCADE'), nullable=False)
+        textbook_id = db.Column(UUID(as_uuid=True), db.ForeignKey('textbooks.textbook_id', ondelete='CASCADE'), nullable=False)
+        meeting_description = db.Column(db.String(255), nullable=False)
+        start_time = db.Column(db.DateTime, nullable=False)
+        end_time = db.Column(db.DateTime, nullable=False)
+        user_address = db.Column(db.String(255), nullable=False)
+        
+        def __init__(self, user_id, textbook_id, meeting_description, start_time, end_time, user_address):
+                self.user_id = user_id
+                self.textbook_id = textbook_id
+                self.meeting_description = meeting_description
+                self.start_time = start_time
+                self.end_time = end_time
+                self.user_address = user_address
+
+# class Rental(db.Model):
+#         __tablename__ = 'rentals'
+
+#         rental_id = db.Column(UUID(as_uuid=True), primary_key=True, default = uuid.uuid4)
+#         user_id = db.Column(UUID(as_uuid=True), db.ForeignKey('users.user_id'), nullable=False)
+#         textbook_id = db.Column(UUID(as_uuid=True), db.ForeignKey('textbooks.textbook_id'), nullable=False)
+#         owner_id = db.Column(UUID(as_uuid=True), db.ForeignKey('users.user_id'), nullable=False)
+#         rental_price = db.Column(db.Numeric(10, 2), nullable=False)
+#         duration = db.Column(db.DateTime, nulllable=False)
+#         due_date = db.Column(db.DateTime, nullable=False)
+#         status = db.Column(db.String(15), default='rented', nullable=False)
+
+#         user = db.relationship('users', foreign_keys=[user_id])
+#         owner = db.relationship('users', foreign_keys=[owner_id])
+#         textbook = db.relationship('Textbook', foreign_keys=[textbook_id])
+
+#         def __init__(self, user_id, textbook_id, owner_id, rental_price, duration, due_date, status='rented'):
+#                 self.user_id = user_id
+#                 self.textbook_id = textbook_id
+#                 self.owner_id = owner_id
+#                 self.rental_price = rental_price
+#                 self.duration = duration
+#                 self.due_date = due_date
+#                 self.status = status
---
+++ textbook_app_schema.sql
@@ -0,0 +1,157 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE TABLE IF NOT EXISTS users(
+    user_id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    username VARCHAR(255) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    phone_number VARCHAR(20) NOT NULL,
+    registration_date TIMESTAMP,
+    profile_picture VARCHAR(255)
+);
+
+CREATE TABLE IF NOT EXISTS textbooks (
+    textbook_id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+    owners_user_id UUID NOT NULL,
+    title VARCHAR(255) NOT NULL,
+    description TEXT NOT NULL,
+    image_url VARCHAR(255),
+    price NUMERIC(10, 2) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (owners_user_id) REFERENCES users(user_id) ON DELETE CASCADE
+);
+
+-- Added user to be the owner of all AI-generated Textbooks
+--Added user to be the owner of all AI generated Textbooks
+INSERT INTO users (first_name, last_name, email, username, password, phone_number, registration_date, profile_picture)
+VALUES
+('John', 'Stones', 'Test@example.com', 'username', 'password', '0000000000' , CURRENT_TIMESTAMP, '/static/images/defaultPFP.png');
+
+INSERT INTO textbooks (owners_user_id, title, description, image_url, price)
+VALUES
+((SELECT user_id FROM users WHERE first_name = 'John'), 'Introduction to Python', 'A beginner''s guide to Python programming.', '/static/images/bookborrowlogo.jpg', 29.99),
+((SELECT user_id FROM users WHERE first_name = 'John'), 'Advanced Python Programming', 'Deep dive into advanced Python concepts.', '/static/images/bookborrowlogo.jpg', 49.99),
+((SELECT user_id FROM users WHERE first_name = 'John'), 'Data Science with Python', 'Learn data science techniques using Python.', '/static/images/bookborrowlogo.jpg', 39.99),
+((SELECT user_id FROM users WHERE first_name = 'John'), 'JavaScript: The Good Parts', 'A concise guide to the best features of JavaScript.', '/static/images/bookborrowlogo.jpg', 34.99),
+((SELECT user_id FROM users WHERE first_name = 'John'), 'JavaScript: The Definitive Guide', 'An in-depth guide to JavaScript programming.', '/static/images/bookborrowlogo.jpg', 44.99),
+((SELECT user_id FROM users WHERE first_name = 'John'), 'The Pragmatic Programmer', 'Tips and techniques for modern software development.', '/static/images/bookborrowlogo.jpg', 42.99),
+((SELECT user_id FROM users WHERE first_name = 'John'), 'Clean Code', 'A handbook of agile software craftsmanship.', '/static/images/bookborrowlogo.jpg', 37.99),
+((SELECT user_id FROM users WHERE first_name = 'John'), 'Design Patterns: Elements of Reusable Object-Oriented Software', 'Classic design patterns for object-oriented programming.', '/static/images/bookborrowlogo.jpg', 49.99),
+((SELECT user_id FROM users WHERE first_name = 'John'), 'Introduction to Machine Learning', 'Basics of machine learning and practical applications.', '/static/images/bookborrowlogo.jpg', 45.99),
+((SELECT user_id FROM users WHERE first_name = 'John'), 'Artificial Intelligence: A Modern Approach', 'Comprehensive overview of AI principles and techniques.', '/static/images/bookborrowlogo.jpg', 59.99),
+((SELECT user_id FROM users WHERE first_name = 'John'), 'Algorithms Unlocked', 'A gentle introduction to algorithms and data structures.', '/static/images/bookborrowlogo.jpg', 31.99),
+((SELECT user_id FROM users WHERE first_name = 'John'), 'The Art of Computer Programming', 'Foundational text on algorithms and programming techniques.', '/static/images/bookborrowlogo.jpg', 69.99),
+((SELECT user_id FROM users WHERE first_name = 'John'), 'Computer Systems: A Programmer''s Perspective', 'Insight into the underlying workings of computer systems.', '/static/images/bookborrowlogo.jpg', 52.99),
+((SELECT user_id FROM users WHERE first_name = 'John'), 'Operating System Concepts', 'Essential concepts of modern operating systems.', '/static/images/bookborrowlogo.jpg', 46.99),
+((SELECT user_id FROM users WHERE first_name = 'John'), 'Database System Concepts', 'Comprehensive guide to database systems and their use.', '/static/images/bookborrowlogo.jpg', 48.99),
+((SELECT user_id FROM users WHERE first_name = 'John'), 'Computer Networking: A Top-Down Approach', 'Introduction to networking concepts from a top-down perspective.', '/static/images/bookborrowlogo.jpg', 41.99),
+((SELECT user_id FROM users WHERE first_name = 'John'), 'Introduction to the Theory of Computation', 'Foundational concepts in computational theory.', '/static/images/bookborrowlogo.jpg', 39.99),
+((SELECT user_id FROM users WHERE first_name = 'John'), 'Elements of Programming Interviews', 'Solutions to challenging programming interview questions.', '/static/images/bookborrowlogo.jpg', 36.99),
+((SELECT user_id FROM users WHERE first_name = 'John'), 'Eloquent JavaScript', 'A modern introduction to JavaScript programming.', '/static/images/bookborrowlogo.jpg', 29.99),
+((SELECT user_id FROM users WHERE first_name = 'John'), 'Python for Data Analysis', 'Using Python for analyzing and visualizing data.', '/static/images/bookborrowlogo.jpg', 32.99),
+((SELECT user_id FROM users WHERE first_name = 'John'), 'Learning SQL', 'An introduction to SQL and database querying.', '/static/images/bookborrowlogo.jpg', 27.99),
+((SELECT user_id FROM users WHERE first_name = 'John'), 'Web Design with HTML, CSS, JavaScript and jQuery Set', 'Comprehensive guide to web design and development.', '/static/images/bookborrowlogo.jpg', 55.99),
+((SELECT user_id FROM users WHERE first_name = 'John'), 'React Up & Running', 'Hands-on guide to building modern web applications with React.', '/static/images/bookborrowlogo.jpg', 39.99);
+
+
+CREATE TABLE IF NOT EXISTS carts(
+    cart_id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+    user_id UUID NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS cart_items (
+    item_id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+    cart_id UUID NOT NULL,
+    textbook_id UUID NOT NULL,
+    checkout_type VARCHAR(10) NOT NULL DEFAULT 'buy',
+    duration INT DEFAULT NONE,
+    quantity INT,
+    FOREIGN KEY (cart_id) REFERENCES carts(cart_id) ON DELETE CASCADE,
+    FOREIGN KEY (textbook_id) REFERENCES textbooks(textbook_id) ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS conversations (
+    conversation_id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+    sender_user_id UUID NOT NULL,
+    receiver_user_id UUID NOT NULL,
+    textbook_id UUID NOT NULL,
+    meetup_location VARCHAR(255),
+    FOREIGN KEY (sender_user_id) REFERENCES users(user_id) ON DELETE CASCADE,
+    FOREIGN KEY (receiver_user_id) REFERENCES users(user_id) ON DELETE CASCADE,
+    FOREIGN KEY (textbook_id) REFERENCES textbooks(textbook_id) ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS messages (
+    message_id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+    user_id UUID NOT NULL,
+    conversation_id UUID NOT NULL,
+    message_text TEXT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE,
+    FOREIGN KEY (conversation_id) REFERENCES conversations(conversation_id) ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS unverified_users (
+    unverified_user_id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    username VARCHAR(255) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    phone_number VARCHAR(20) NOT NULL,
+    registration_date TIMESTAMP,
+    profile_picture VARCHAR(255)
+);
+
+CREATE TABLE IF NOT EXISTS verification_codes (
+    code_id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+    user_id UUID NOT NULL,
+    verification_code VARCHAR(255) NOT NULL,
+    expiration_timestamp TIMESTAMP NOT NULL,
+    is_used BOOLEAN DEFAULT FALSE, 
+    FOREIGN KEY (user_id) REFERENCES unverified_users(unverified_user_id) ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS orders (
+    order_id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+    user_id UUID NOT NULL,
+    status VARCHAR(10) NOT NULL DEFAULT 'pending',
+    price NUMERIC(10, 2) NOT NULL,
+    order_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS order_items (
+    item_id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+    order_id UUID NOT NULL,
+    textbook_id UUID NOT NULL,
+    due_date TIMESTAMP,
+    quantity INT NOT NULL,
+    FOREIGN KEY (order_id) REFERENCES orders(order_id) ON DELETE CASCADE,
+    FOREIGN KEY (textbook_id) REFERENCES textbooks(textbook_id) ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS ratings (
+    rating_id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+    textbook_id UUID NOT NULL,
+    user_id UUID REFERENCES users(user_id),
+    rating INT CHECK (rating >= 1 AND rating <= 5),
+    comment TEXT,
+    FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE,
+    FOREIGN KEY (textbook_id) REFERENCES textbooks(textbook_id) ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS meetups (
+    meeting_id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+    user_id UUID, 
+    textbook_id UUID,
+    meeting_description VARCHAR(255) NOT NULL,
+    start_time TIMESTAMP NOT NULL,
+    end_time TIMESTAMP NOT NULL,
+    user_address VARCHAR(255) NOT NULL,       
+    FOREIGN KEY (textbook_id) REFERENCES textbooks(textbook_id) ON DELETE CASCADE,
+    FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE
+);
